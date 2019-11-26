acd5f889f1a29335c25546ae07e4dca8708bf467
Greg <gmerrill03@gmail.com>
2019-11-25 20:28:50 -0800

    Continue work on search bar

--- react-frontend/src/components/SearchBar.js
+++ react-frontend/src/components/SearchBar.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './css/SearchBar.scss';
 
-const SearchBar = () => (
-  <div className='ui input SearchBar'>
-    <input placeholder='Search HONY stories' type='text' value='' />
-  </div>
-);
+const SearchBar = () => {
+  const [query, setQuery] = useState('');
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(query);
+  };
 
+  const handleChange = evt => setQuery(evt.target.value);
 
+  return (
+    <form className='SearchBar-form' onSubmit={handleSubmit}>
+      <input className='SearchBar-input' placeholder='Search HONY stories' type='text' value={query} onChange={handleChange} />
+    </form>
+  );
+}
 
 export default SearchBar;
